0e8e9883b2698b642ab1e0a64515ce7a6db71267
Andy Smith <andyrsmith@googlemail.com>
2016-01-03 22:23:22 +0000

    Incremental changes....

--- init.el
+++ init.el
@@ -1,51 +1,46 @@
-;; (set-frame-font "misc fixed 12")
+(set-frame-font "Fixed 10")
 
-;;(set-frame-font "-misc-fixed-medium-r-normal--10-*-100-100-c-60-iso8859-1" )
-(set-frame-font "-unknown-Liberation Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
-;; Org favourite
 ;; (set-frame-font "-unknown-Liberation Mono-normal-normal-normal-*-20-*-*-*-m-0-iso10646-1")
 ;; (set-frame-font "-misc-fixed-medium-r-normal--10-*-75-75-c-60-iso8859-8")
 ;; (set-frame-font "-misc-fixed-medium-r-normal--6-*-75-75-c-70-iso8859-5")
-;; (set-frame-font "-misc-fixed-medium-r-normal--11-*-100-100-c-80-iso8859-8")
-
-;;(set-frame-font "Misc Fixed 8")
-
 ;; (set-frame-font "-misc-fixed-medium-r-semicondensed--13-*-75-75-c-60-iso8859-15")
-;; (set-frame-font "-misc-fixed-medium-r-normal--15-*-75-75-c-90-iso8859-16")
 ;; (set-frame-font "-urw-Nimbus Mono L-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1")
 ;; (set-frame-font "-urw-Nimbus Mono L-bold-normal-normal-*-*-*-*-*-m-0-iso10646-1")
 ;; (set-frame-font "-unknown-Liberation Mono-normal-normal-normal-*-10-*-*-*-m-0-iso10646-1")
 ;; (set-frame-font "-unknown-Liberation Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
-;; (set-frame-font "-unknown-Liberation Mono-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
 ;; (set-frame-font "-unknown-Liberation Mono-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1")
 ;; (set-frame-font "-unknown-Liberation Mono-normal-normal-normal-*-21-*-*-*-m-0-iso10646-1")
 ;; (set-frame-font "-unknown-Liberation Mono-normal-normal-normal-*-22-*-*-*-m-0-iso10646-1")
 ;; (set-frame-font "-unknown-Liberation Mono-normal-normal-normal-*-23-*-*-*-m-0-iso10646-1")
 ;; (set-frame-font "-unknown-Liberation Mono-normal-normal-normal-*-40-*-*-*-m-0-iso10646-1")
-
-;; (set-frame-font "Misc Fixed 12")
-(set-frame-font "Liberation Mono 10")
-
-;;(add-to-list 'load-path "~/.emacs.d/python-mode")
-;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (set-frame-font "-unknown-Liberation Mono-normal-normal-normal-*-80-*-*-*-m-0-iso10646-1")
+;; (set-frame-font "-unknown-Liberation Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+;; (set-frame-font "Fixed 10")
+;; (set-frame-font "Liberation Mono 12")
+;; (set-frame-font "-unknown-Liberation Mono-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+;;(set-frame-font "-misc-fixed-medium-r-normal--15-*-75-75-c-90-iso8859-16")
+(set-frame-font "-misc-fixed-medium-r-normal--11-*-100-100-c-80-iso8859-8")
 
 (require 'compile)
 (require 'package)
+
 (add-to-list 'package-archives '("melpa"     . "http://melpa.org/packages/") )
 
-;;(add-to-list 'package-archives '("melpa-stable"     . "http://stable.melpa.org/packages/") )
-;;(add-to-list 'load-path "~/.emacs.d/vendor/swank-js")
-;;(add-to-list 'load-path "~/.emacs.d/vendor/slime")
-;;(add-to-list 'load-path "~/.emacs.d/python-mode")
+(setq auto-mode-alist
+      (cons '("SConstruct" . python-mode) auto-mode-alist))
+(setq auto-mode-alist
+      (cons '("SConscript" . python-mode) auto-mode-alist))
 
-;;(cons 1 '(2 3 4 )) 
-;;(add-to-list 'load-path "~/.emacs.d")
 
 (defun slurp (x)
   (with-temp-buffer 
-	 ign
-	 (insert-file-contents x)
-	 (buffer-string) ) )
+    ign
+    (insert-file-contents x)
+    (buffer-string)))
+
+(defun debug-discovery ()
+  (interactive)
+  (gdb "gdb -i=mi -nx -x /home/andy/discovery.gdbinit"))
 
 (global-set-key (kbd "C-x C-o") 'ff-find-other-file)
 
@@ -53,8 +48,20 @@
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-c C-h C-s") 'helm-swoop)
 (global-set-key (kbd "C-c C-h C-l") 'helm-locate)
+(global-set-key (kbd "C-c C-h C-a") 'helm-do-ag)
+
+(global-set-key (kbd "M-g M-f") 'helm-gtags-find-files)
+(global-set-key (kbd "M-g M-t") 'helm-gtags-find-tag)
+(global-set-key (kbd "M-g M-r") 'helm-gtags-find-rtag)
+
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x C-b") 'helm-mini)
+
+
+
+
+
+;;(global-set-key (kbd "C-x C-b") 'helm-mini)
 
 (global-set-key (kbd "C-x r b") 'helm-bookmarks)
 (global-set-key (kbd "M-x") 'helm-M-x)
@@ -63,18 +70,18 @@
 (global-set-key (kbd "C-c C-m C-s" ) 'magit-status)
 ;; For d
 (add-to-list 'compilation-error-regexp-alist
-		'("^object\.Exception@\\(.*\\)(\\([0-9]+\\)).*"
-		  1 2 ) )
+             '("^object\.Exception@\\(.*\\)(\\([0-9]+\\)).*"
+               1 2))
 
 ;; For d
 (add-to-list 'compilation-error-regexp-alist
-		'("^core\.exception\.AssertError@\\(.*\\)(\\([0-9]+\\)).*"
-		  1 2 ) )
+             '("^core\.exception\.AssertError@\\(.*\\)(\\([0-9]+\\)).*"
+               1 2 ) )
 
 ;; For node.js
 (add-to-list 'compilation-error-regexp-alist
-				 '("^.*at.*(\\(.*\\):\\([0-9]+\\):.*"
-		  1 2 ) )
+             '("^.*at.*(\\(.*\\):\\([0-9]+\\):.*"
+               1 2 ) )
 ;; (setq compilation-error-regexp-alist (cdr compilation-error-regexp-alist))
 
 (add-to-list
@@ -84,7 +91,7 @@
 
 (defun revert-buffer-with-prejudice () 
   (interactive) 
-  (revert-buffer t t )
+  (revert-buffer 't 't )
   )
 
 (global-set-key (kbd "C-c r")  'revert-buffer-with-prejudice)
@@ -95,19 +102,12 @@
 (defun find-file-in-clipboard () 
   (interactive)
   (find-file-at-point (x-get-clipboard)))
+
 (global-set-key (kbd "C-c p")  'find-file-in-clipboard)
 
 (defun do-revert () 
   (interactive) 
   (revert-buffer nil 't))
-
-
-;;(require 'org-crypt)
-;;(org-crypt-use-before-save-magic)
-;;(setq org-tags-exclude-from-inheritance (quote ("crypt")))
-;; GPG key to use for encryption
-;; Either the Key ID or set to nil to use symmetric encryption.
-;;(setq org-crypt-key "9059B882D50776AA")
 
 (defun revert-buffer-with-prejudice () 
   (interactive) 
@@ -117,6 +117,37 @@
 
 (package-initialize)
 
+
+(eval-after-load "haskell-mode"
+  '(progn
+     (define-key haskell-mode-map (kbd "C-x C-d") nil)
+     (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+     (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+     (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+     (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+     (define-key haskell-mode-map (kbd "C-c M-.") nil)
+     (define-key haskell-mode-map (kbd "C-c C-d") nil)))
+
+(require 'haskell-interactive-mode)
+(require 'haskell-process)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-basic-offset 4)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(org-agenda-files (quote ("~/Dropbox/gtd/gtd.org")))
+ '(org-directory "~/Dropbox/gtd")
+ '(org-hide-leading-stars t)
+ '(python-shell-interpreter "ipython")
+ '(python-shell-interpreter-args "--pylab=qt")
+ '(tab-width 4))
 (defun switch-to-org ()
   (interactive)
   (switch-to-buffer "gtd.org")
@@ -154,7 +185,6 @@
   (interactive)
   (switch-to-buffer "*shell*"))
 
-
 (windmove-default-keybindings 'meta)
 
 ;; (elpy-enable)
@@ -163,8 +193,6 @@
     (when the-plist
       (cons (car the-plist) (cadr the-plist))))
   (let ((alist '()))
-
-
     (while the-plist
       (add-to-list 'alist (get-tuple-from-plist the-plist))
       (setq the-plist (cddr the-plist)))
@@ -176,11 +204,6 @@
 (color-theme-initialize)
 
 (require 'color-theme)
-;;(color-theme-deep-blue)
-;;(color-theme-hober)
-;;(color-theme-cobalt)
-;;(color-theme-clarity)
-
 (color-theme-midnight)
 
 (global-set-key [f1] 'wg-switch-to-workgroup)
@@ -196,8 +219,6 @@
 
 (windmove-default-keybindings 'meta)
 
-;; (require 'color-theme)
-;; (elpy-enable)
 (defun plist-to-alist (the-plist)
   (defun get-tuple-from-plist (the-plist)
     (when the-plist
@@ -219,12 +240,10 @@
 (defun reboot-python ()
   (interactive)
   (save-current-buffer
-    ; Disable querying while we delete.|
+                                        ; Disable querying while we delete.|
     (let ( kill-buffer-query-functions '() )
       (if (get-buffer "*Python*") (kill-buffer "*Python*") )
       (if (get-buffer "*Jython*") (kill-buffer "*Jython*") ))))
-
-
 
 (defun unfill-paragraph (&optional region)
   "Takes a multi-line paragraph and makes it into a single line of text."
@@ -234,29 +253,11 @@
 
 (global-set-key (kbd "M-Q") 'unfill-paragraph)
 
-;;(define-key global-map "\M-Q" 'unfill-paragraph)
-;; Put in thegge front - have had issues/conflicts with elpa :-(
-
-;;(require 'python-mode)
-
 (defun insert-hg-ignore () 
   (interactive)
   (insert "`hg root`/.hgignore"))
 
 (global-set-key (kbd "C-c C-h C-g C-i" ) 'insert-hg-ignore)
-
-
-;; (require 'python-mode)
-;; (setq load-path (cons "/home/andy/.emacs.d/python-mode" load-path))
-;; (setq load-path (cons "/home/andy/.emacs.d/ipython"     load-path))
-
-;;(add-to-list 'auto-mode-alist '("SConstruct" . python-mode) )
-;;(add-to-list 'auto-mode-alist '("SConscript" . python-mode) )
-;;(add-to-list 'auto-mode-alist '("\\.py$" . python-mode) )
-
-;;(setq interpreter-mode-alist (cons '("python" . python-mode)
-;;				   interpreter-mode-alist))
-;;(autoload 'python-mode "python-mode" "Python editing mode." t)
 
 (require 'workgroups)
 (setq wg-prefix-key (kbd "C-c w"))
@@ -267,57 +268,27 @@
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 
-;; (defun set-auto-complete-as-completion-at-point-function ()
-;;   (setq wcompletion-at-point-functions '(auto-complete)))
-;; (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-comple
-;;tion-at-point-function)
-;; (add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-;; (add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
-
-;;(setq py-install-directory "/home/andy/.emacs.d/python-mode.el-6.1.1")
-;;(add-to-list 'load-path py-install-directory)
-;;(require 'python-mode)
-;;(require 'python-mode)
-;;(require 'ipython)
-;;(color-theme-solarized-darkp)
-;; (elpy-enable)
-;; (elpy-use-ipython)
-
-; supercede with helm
-;(ido-mode 't)  
-
 (require 'flymake-haskell-multi)
 (autoload 'ghc-init "ghc" nil t)
 (add-hook 'haskell-mode-hook (lambda () (ghc-init) ))
 (add-hook 'haskell-mode-hook 'flymake-haskell-multi-load)
 
 (setq-default indent-tabs-mode nil)
-;;(require 'ipython)
-;;(add-to-list 'load-path "/path/to/js2-mode/directory")
 
 (require 'slime-autoloads)
-;;(require 'slime-autoloads)
-
-;(require 'slime-js)
-;(slime-setup '(slime-js))
 
 (slime-setup '(slime-fancy))
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-;(global-set-key [f5] 'slime-js-reload)
-;; (add-hook 'js2-mode-hook
-;;           (lambda ()
-;;             (slime-js-minor-mode 1)))
-
+(add-to-list 'auto-mode-alist '("\\.ice$" . idl-mode))
 
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-(require 'auto-complete-config)
 (ac-config-default)
 
 (require 'ac-slime)
-(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-mode-hook      'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 
 (eval-after-load "auto-complete"
@@ -334,23 +305,23 @@
 (global-set-key "\C-cb" 'org-iswitchb)
 
 (setq org-capture-templates '(
-			      ("t" "Todo" entry (file+headline "~/Dropbox/gtd/gtd.org" "Tasks")
-			       "* TODO %?\n%F")
+                              ("t" "Todo" entry (file+headline "~/Dropbox/gtd/gtd.org" "Tasks")
+                               "* TODO %?\n%F")
                               
-			      ("r" "Todo" entry (file+headline "~/Dropbox/gtd/gtd.org" "Tasks")
+                              ("r" "Todo" entry (file+headline "~/Dropbox/gtd/gtd.org" "Tasks")
                                "* TODO %?\n  %i\n  %a")
                               
-			      ("r" "Todo" entry (file+headline "~/Dropbox/gtd/gtd.org" "Tasks")
-			       "* TODO %?\n%F")
+                              ("r" "Todo" entry (file+headline "~/Dropbox/gtd/gtd.org" "Tasks")
+                               "* TODO %?\n%F")
                               
-			      ("p" "Performance" entry (file+headline "~/Dropbox/gtd/gtd.org" "Performance")
-			       "* TODO %?\n")
+                              ("p" "Performance" entry (file+headline "~/Dropbox/gtd/gtd.org" "Performance")
+                               "* TODO %?\n")
                               
-			      ("s" "Schedule" entry (file+headline "~/Dropbox/gtd/gtd.org" "Schedule")
-			       "* %?\n")
+                              ("s" "Schedule" entry (file+headline "~/Dropbox/gtd/gtd.org" "Schedule")
+                               "* %?\n")
                               
-			      ("j" "Journal" entry (file+datetree "~/Dropbox/gtd/journal.org")
-			       "* %U %?")))
+                              ("j" "Journal" entry (file+datetree "~/Dropbox/gtd/journal.org")
+                               "* %U %?")))
 
 
 (global-set-key (kbd "C-c s") 'ispell)
@@ -365,57 +336,37 @@
 
 (require 'semantic)
 
-;; Enable EDE (Project Management) features
-;; (global-ede-mode 1)
-;; (semantic-load-enable-excessive-code-helpers)      ; Enable prototype help and smart completion
-
 (setq helm-semantic-fuzzy-match t
       helm-imenu-fuzzy-match    t)
-
-
 
 ;; TODO - These two broken I think . Problem with the lambda
 (defun compile-in-buffer (cmd buf)
   (interactive)
-  (let* ((compilation-buffer-name-function (lambda () buf) ) )
+  (let* ((compilation-buffer-name-function (lambda (x) buf) ) )
     (compile cmd)))
 
 (defun compile-dev ()
   (interactive)
-  (compile-in-buffer "cd ~/repos/dev/cpp && scons" "dev")
-  )
+  (compile-in-buffer "cd ~/repos/dev/cpp && scons" "dev"))
 
-;; (compile-dev)
+(defun rangeExperiment ()
+  (interactive)
+  (compile-in-buffer "cd ~/repos/pingu && rdmd -unittest rangeExperiment.d" "rangeExperiment"))
 
 
-;;(global-srecode-minor-mode 1)            ; Enable template insertion menu
+(defun pingu ()
+  (interactive)
+  (compile-in-buffer "cd ~/repos/pingu && rdmd -unittest --main -version=diagnostic Order.d" "pingu"))
 
-    ;; (add-hook 'c++-mode-hook 'irony-mode)
-    ;; (add-hook 'c-mode-hook 'irony-mode)
-    ;; (add-hook 'objc-mode-hook 'irony-mode)
-    ;; replace the `completion-at-point' and `complete-symbol' bindings in
-    ;; irony-mode's buffers by irony-mode's asynchronous function
-    ;; (defun my-irony-mode-hook ()
-    ;;   (define-key irony-mode-map [remap completion-at-point]
-    ;;     'irony-completion-at-point-async)
-    ;;   (define-key irony-mode-map [remap complete-symbol]
-    ;;     'irony-completion-at-point-async))
-    ;; (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(global-set-key (kbd "C-c C-h C-p") 'pingu)
+(defun pingu ()
+  (interactive)
+  (compile-in-buffer "cd ~/repos/pingu && rdmd Order.d" "pingu"))
 
-    ;; Only needed on Windows
-    (when (eq system-type 'windows-nt)
-      (setq w32-pipe-read-delay 0))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Dropbox/gtd/gtd.org")))
- '(org-directory "~/Dropbox/gtd")
- '(org-hide-leading-stars t)
- '(python-shell-interpreter "ipython")
- '(python-shell-interpreter-args "--pylab=qt"))
+(add-to-list 'exec-path "/home/andy/bin")
+
+
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -423,5 +374,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-
