8149cfa8180a680a59bf7458fa07a9ba62322fb3
Andy Smith <andyrsmith@gmail.com>
2019-11-15 04:47:30 +0000

    Add spit function.

--- init.el
+++ init.el
@@ -135,6 +135,13 @@
     (insert-file-contents x)
     (buffer-string)))
 
+(defun spit (fn x)
+  "Write file!!"
+  (with-temp-buffer
+    (insert x)
+    (message "Got this far")
+    (write-file fn)))
+
 (defun debug-discovery ()
   "Debug the discovery app."
   (interactive)
@@ -922,6 +929,8 @@ with micros, seconds, nanos etc. Display result using 'message' if successful"
  '(flycheck-c/c++-clang-executable "clang-5.0")
  '(flycheck-clang-args (quote ("-xc++")))
  '(flycheck-clang-language-standard "c++14")
+ '(fountain-export-font "Courier New")
+ '(fountain-export-include-scene-numbers t)
  '(gdb-many-windows t)
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
@@ -1010,7 +1019,7 @@ with micros, seconds, nanos etc. Display result using 'message' if successful"
    (quote
     (olivetti unicode-fonts hy-mode org-kanban nhexl-mode dracula-theme slime ob-kotlin amd-mode sed-mode ranger thrift doom-themes aggressive-indent meson-mode ace-mc helm-org-rifle elixir-mode dfmt ubuntu-theme f3 f org-mobile-sync company-dcd dirtree direx indium flymake-cursor darcula-theme typescript-mode go julia-shell julia-repl julia-mode flycheck-kotlin erlang google-this py-autopep8 flymake-python-pyflakes haskell-mode editorconfig flycheck-clang-tidy kotlin-mode erc-view-log color-theme-sanityinc-solarized color-theme-solarized scala-mode helm-unicode cmake-mode nim-mode json-rpc restclient workgroups2 gnuplot gnuplot-mode orgtbl-ascii-plot forth-mode csv-mode git-gutter rjsx-mode org-present json-mode d-mode ponylang-mode flycheck-pony cider clojure-mode wrap-region multiple-cursors ag helm-projectile dumb-jump helm-cscope ein elpy swift3-mode yaml-mode workgroups web-mode utop tuareg tide switch-window swiper-helm solarized-theme sml-mode smex skewer-mode scala-mode2 sass-mode rust-mode rtags rainbow-delimiters quack pylint protobuf-mode paredit org nyan-mode nurumacs nasm-mode monokai-theme monky markdown-mode magit less-css-mode jsx-mode js3-mode jedi jade-mode ido-ubiquitous iasm-mode helm-swoop helm-package helm-gtags helm-dash helm-company helm-cider helm-ag groovy-mode graphviz-dot-mode go-mode ghci-completion ghc-imported-from ghc ggtags geiser fsharp-mode fountain-mode flycheck-pyflakes flycheck-irony flycheck-haskell find-file-in-project ensime elm-mode edts dash-functional dart-mode csv-nav csharp-mode coffee-mode clang-format caroline-theme caml auctex ace-jump-mode ac-slime ac-helm ac-haskell-process ac-clang ac-cider abyss-theme 2048-game)))
  '(projectile-tags-backend (quote ggtags))
- '(python-shell-interpreter "python3.8")
+ '(python-shell-interpreter "ipython")
  '(python-shell-interpreter-args "--simple-prompt --pylab")
  '(safe-local-variable-values
    (quote
