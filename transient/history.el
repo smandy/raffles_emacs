637488066448eada50dbd1038738dbd6ae8a2739
Andy Smith <andyrsmith@gmail.com>
2022-01-13 16:49:37 +0000

    Add code for intellij idea bindings. Wasn't a logical place to put them.

--- transient/history.el
+++ transient/history.el
@@ -1,7 +1,9 @@
-((magit-commit nil)
+((magit-branch nil)
+ (magit-commit nil)
  (magit-gitignore nil)
  (magit-log
   ("-n256" "--graph" "--decorate"))
  (magit-pull nil)
  (magit-push
-  ("--force-with-lease")))
+  ("--force-with-lease"))
+ (magit-rebase nil))
