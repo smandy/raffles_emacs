3fa0a484f259e63c45a04d6f0cea4aa405df69d7
Andy Smith <andyrsmith@gmail.com>
2023-12-22 19:39:52 +0000

    Add more current example.

--- init.el
+++ init.el
@@ -1031,7 +1031,7 @@ with micros, seconds, nanos etc. Display result using 'message' if successful"
                (isofmt  (format-time-string "%Y-%m-%dT%H:%M:%S.%N" (seconds-to-time seconds))))
           (message (format "%s (%s) -> %s" x prefix isofmt))))))
 
-
+; (parse-epoch-time "1703273132.631212") "1703273132.631212 (s) -> 2023-12-22T19:25:32.631211996"
 ; (parse-epoch-time "1482672627.025747002" ) "1482672627.025747 (s) -> 2016-12-25T13:30:27.025747060"
 ; (parse-epoch-time "1482672627025.747023" ) "1482672627025.747 (ms) -> 2016-12-25T13:30:27.025747060"
 ; (parse-epoch-time "1482672627025747.032" ) "1482672627025747.0 (µs) -> 2016-12-25T13:30:27.025747060"
